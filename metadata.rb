7994d583ed61dbb820ef2e724ddc79a90ea45a51
Gleb M Borisov <bgleb@yandex-team.ru>
2013-04-25 16:56:28 +0400

    metadata.rb added

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             "partial_search"
+maintainer       "John Keiser"
+maintainer_email "john@johnkeiser.com"
+license          "Apache 2.0"
+description      "making partial searches easy"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
